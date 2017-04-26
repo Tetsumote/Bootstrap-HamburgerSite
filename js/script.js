5e2ab4f2b6df9bd42ac6a1c99255065e12aa154f
tetsumote <unionproject88@gmail.com>
2017-04-26 12:02:56 +0200

    end of scrollspy

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
 $(function(){
 'use strict';
+
 	
 $('#featured').carousel({
   interval: false
+});		
+	
+	
+	
+var topoffset = 50;		//offset to scrollspy method
+//Activate Scroll spy	
+$('body').scrollspy({
+	target: "header .navbar",
+	offset: topoffset
+});
+  var active = $('body').find('li.active a').attr('href');	
+	
+	if(active !== '#featured'){
+		$('header nav').addClass('inbody');
+	}else{
+		$('header nav').removeClass('inbody');
+}
+
+
+$('.navbar-fixed-top').on('activate.bs.scrollspy', function () {
+  var active = $('body').find('li.active a').attr('href');	
+	
+	if(active !== '#featured'){
+		$('header nav').addClass('inbody');
+	}else{
+		$('header nav').removeClass('inbody');
+}
 });	
-	
-	
 });
