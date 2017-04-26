684d06171b712913b207365835467b19ed69e35c
tetsumote <unionproject88@gmail.com>
2017-04-26 14:42:40 +0200

    end of menu style

--- js/script.js
+++ js/script.js
@@ -5,7 +5,20 @@ $(function(){
 $('#featured').carousel({
   interval: false
 });		
-	
+$('.navbar a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === 
+      this.pathname.replace(/^\//,'') && 
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling	
 	
 	
 var topoffset = 50;		//offset to scrollspy method
